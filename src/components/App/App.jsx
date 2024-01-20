df510dab491271483f564f28de59ae4a4e4f0ba8
lukblan <luk.blanko@gmail.com>
2024-01-20 18:09:57 -0300

    Add preview button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,25 @@
 import { Form } from '../Form/Form';
 import { TabSection } from '../TabSection/TabSection';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [hiddenButton, setHidden] = useState(false);
+  const buttonClass = `show-preview-button ${hiddenButton ? 'hide' : ''}`;
+
+  const toggleHide = () => setHidden(!hiddenButton);
+
   return (
     <>
       <Form />
+      <button className={buttonClass} onClick={toggleHide}>
+        Show
+      </button>
       <main>
         <TabSection />
         <div className="preview-section">Example</div>
       </main>
+
     </>
   );
 }
