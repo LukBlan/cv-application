18189b9a15abd35593d3b8ae16bb49bb91021346
lukblan <luk.blanko@gmail.com>
2024-01-22 14:14:18 -0300

    Add svg button to form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,7 +4,9 @@ function Form() {
   return (
     <form className="download-bar">
       <input type="text" placeholder="Untitled Resume" className="download-file-name" />
-      <button>Download</button>
+      <button className="download-button">
+        <img src="/img/download.svg" alt="download resume" />
+      </button>
     </form>
   );
 }
